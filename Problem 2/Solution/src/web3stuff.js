5ada5c3fa7d6f63c77815d57e622470729bc51c8
Chris Burgdorff <wesborland1234@aol.com>
2022-08-19 17:02:23 -0400

    took out private key

--- Problem 2/Solution/src/web3stuff.js
+++ Problem 2/Solution/src/web3stuff.js
@@ -5,7 +5,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider(
 
 const beatlesAddress = "0xaE26B69F28b977e9A4C95788F85CaE039896d2Bc";
 const senderAddress = "0x263B9a8E742E577D0D8E3391e6a0f738cAdda9f9";
-const privateKey = "f56e4f3d8cc32ce1958e576aeeb741636e0b9a46bab185d89a8d120a4e424ebd";
+const privateKey = "";
 
 const beatlesABI = [
   {
